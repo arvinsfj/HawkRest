e534a40a4b9fe9e015488025a4fa6d25f76c9af7
arvin.sfj <arvin.sfj@gmail.com>
2016-02-29 21:41:53 +0800

    Simplify HKRest Code.

--- HawkRest/HawkRest/Core/HKRest.swift
+++ HawkRest/HawkRest/Core/HKRest.swift
@@ -54,52 +54,52 @@ class HKRest {
     
     class func head(config: ParameterRequest) -> HKCore
     {
-        let _config: HKParameterRequest = self.getConfig(HKParameterRequest(), config:config);
+        let _config = getConfig(HKParameterRequest(), config:config);
         return HKCore(httpMethod: .HEAD, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, parameters: _config.parameters);
     }
     class func get(config: ParameterRequest) -> HKCore
     {
-        let _config: HKParameterRequest = self.getConfig(HKParameterRequest(), config:config);
+        let _config = getConfig(HKParameterRequest(), config:config);
         return HKCore(httpMethod: .GET, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, parameters: _config.parameters);
     }
     class func post(config: ParameterRequest) -> HKCore
     {
-        let _config: HKParameterRequest = self.getConfig(HKParameterRequest(), config:config);
+        let _config = getConfig(HKParameterRequest(), config:config);
         return HKCore(httpMethod: .POST, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, parameters: _config.parameters);
     }
     class func post(config: BodyRequest) -> HKCore
     {
-        let _config: HKBodyRequest = self.getConfig(HKBodyRequest(), config:config);
+        let _config = getConfig(HKBodyRequest(), config:config);
         return HKCore(httpMethod: .POST, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, body: _config.body);
     }
     class func put(config: ParameterRequest) -> HKCore
     {
-        let _config: HKParameterRequest = self.getConfig(HKParameterRequest(), config:config);
+        let _config = getConfig(HKParameterRequest(), config:config);
         return HKCore(httpMethod: .PUT, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, parameters: _config.parameters);
     }
     class func put(config: BodyRequest) -> HKCore
     {
-        let _config: HKBodyRequest = self.getConfig(HKBodyRequest(), config:config);
+        let _config = getConfig(HKBodyRequest(), config:config);
         return HKCore(httpMethod: .PUT, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, body: _config.body);
     }
     class func patch(config: ParameterRequest) -> HKCore
     {
-        let _config: HKParameterRequest = self.getConfig(HKParameterRequest(), config:config);
+        let _config = getConfig(HKParameterRequest(), config:config);
         return HKCore(httpMethod: .PATCH, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, parameters: _config.parameters);
     }
     class func patch(config: BodyRequest) -> HKCore
     {
-        let _config: HKBodyRequest = self.getConfig(HKBodyRequest(), config:config);
+        let _config = getConfig(HKBodyRequest(), config:config);
         return HKCore(httpMethod: .PATCH, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, body: _config.body);
     }
     class func delete(config: ParameterRequest) -> HKCore
     {
-        let _config: HKParameterRequest = self.getConfig(HKParameterRequest(), config:config);
+        let _config = getConfig(HKParameterRequest(), config:config);
         return HKCore(httpMethod: .DELETE, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, parameters: _config.parameters);
     }
     class func delete(config: BodyRequest) -> HKCore
     {
-        let _config: HKBodyRequest = self.getConfig(HKBodyRequest(), config:config);
+        let _config = getConfig(HKBodyRequest(), config:config);
         return HKCore(httpMethod: .DELETE, url: _config.url, headers: _config.headers, username: _config.username, password: _config.password, body: _config.body);
     }
 }
